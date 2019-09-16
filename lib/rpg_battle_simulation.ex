5d79ac559cf4e42ea2a3a6370ccceb7ea247b0c2
Olga Draber <olgadraber@gmail.com>
2019-09-16 21:45:00 +0200

    add function for finding best opponent values for given results probability

--- lib/rpg_battle_simulation.ex
+++ lib/rpg_battle_simulation.ex
@@ -13,29 +13,28 @@ defmodule RpgBattleSimulation do
     ]
   }
 
-  def test_group_1 do
-    defender = %{
-      name: "group 1",
-      tactics: [3, 1],
-      leadership: [2, 1],
-      army_size: 400,
-      heroes: [[4, 4], [4, 3], [4, 4]]
-    }
+  @group_1 %{
+    name: "group 1",
+    tactics: [3, 1],
+    leadership: [2, 1],
+    army_size: 400,
+    heroes: [[4, 4], [4, 3], [4, 4]]
+  }
 
-    Logic.start(@nilfgaard_small_army, defender)
-    |> do_next_round_until_finished()
-  end
+  @group_2 %{
+    name: "group 1",
+    tactics: [2, 1],
+    leadership: [3, 2],
+    army_size: 400,
+    heroes: [[4, 4], [4, 4], [3, 4], [3, 4]]
+  }
 
-  def test_group_2 do
-    defender = %{
-      name: "group 1",
-      tactics: [2, 1],
-      leadership: [3, 2],
-      army_size: 400,
-      heroes: [[4, 4], [4, 4], [3, 4], [3, 4]]
-    }
+  def nilfgaard_small_army, do: @nilfgaard_small_army
+  def group_1, do: @group_1
+  def group_2, do: @group_2
 
-    Logic.start(@nilfgaard_small_army, defender)
+  def test_run(defender, attacker \\ @nilfgaard_small_army) do
+    Logic.start(attacker, defender)
     |> do_next_round_until_finished()
   end
 
@@ -47,10 +46,10 @@ defmodule RpgBattleSimulation do
 
   defp do_next_round_until_finished(battle), do: battle
 
-  def simulate_probability(function, simulations \\ 1_000) do
+  def simulate_probability(defender, attacker \\ @nilfgaard_small_army, simulations \\ 1_000) do
     1..simulations
     |> Flow.from_enumerable()
-    |> Flow.map(fn _ -> function.() end)
+    |> Flow.map(fn _ -> test_run(defender, attacker) end)
     |> Flow.partition()
     |> Flow.reduce(
       fn -> %{} end,
@@ -71,10 +70,11 @@ defmodule RpgBattleSimulation do
       & &1
     )
     |> Enum.to_list()
+    |> List.first()
     |> format_data(simulations)
   end
 
-  defp format_data([results], simulations) do
+  defp format_data(results, simulations) do
     results
     |> Enum.map(fn {result, data} ->
       total_value = data |> Enum.map(fn {_, k} -> k end) |> Enum.sum()
@@ -83,10 +83,53 @@ defmodule RpgBattleSimulation do
 
       {result, %{chance: total_percentage, rounds: rounds_percentage}}
     end)
+    |> Map.new()
   end
 
   defp to_percentage(value, total) do
-    result = (value * 1.0 / total * 100) |> Float.round(2)
-    "#{result}%"
+    (value * 1.0 / total * 100) |> Float.round(2)
+  end
+
+  def find_best_values_for_probability(
+        defender,
+        attacker_won_desired_probability,
+        simulations \\ 100
+      ) do
+    1..simulations
+    |> Flow.from_enumerable()
+    |> Flow.map(fn _ ->
+      attacker_input_data =
+        @nilfgaard_small_army
+        |> Map.put(:tactics, random_skills())
+        |> Map.put(:leadership, random_skills())
+        |> Map.put(:heroes, [random_skills(), random_skills(), random_skills()])
+
+      {attacker_input_data, simulate_probability(defender, attacker_input_data)}
+    end)
+    |> Flow.partition()
+    |> Flow.reduce(
+      fn -> %{} end,
+      fn {input_data, simulation_result}, acc ->
+        case simulation_result["attacker_won"][:chance] do
+          percentage
+          when percentage <= attacker_won_desired_probability + 5 and
+                 percentage >= attacker_won_desired_probability - 5 ->
+            Map.put(acc, percentage, input_data)
+
+          _ ->
+            acc
+        end
+      end
+    )
+    |> Flow.departition(&Map.new/0, &Map.merge/2, & &1)
+    |> Enum.to_list()
+    |> List.first()
+  end
+
+  defp random_skills() do
+    dices = :rand.uniform(4)
+    skill = :rand.uniform(5) - 1
+
+    [dices, skill]
   end
 end
